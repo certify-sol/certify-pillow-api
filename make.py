52defc538156abac7fa660200b376d63283b4f34
Pulkit Garg <pulkitgarg784@gmail.com>
2021-09-03 20:05:46 +0530

    Update make.py

--- make.py
+++ make.py
@@ -1,4 +1,9 @@
 from PIL import Image, ImageFont, ImageDraw
+import qrcode
+from qrcode.image.styledpil import StyledPilImage
+from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
+from qrcode.image.styles.colormasks import SolidFillColorMask
+
 
 heading = "Certificate for Marriage"
 lines = [
@@ -9,7 +14,7 @@ lines = [
     "on the 19th day of July in the year 2021",
     "by Adolf Hitler"
 ]
-
+qr_link = '2HyPunEH73PUdrFmEKUdaKPJajq11sB9uzzpVoaqkt6LZFthmysHKjRSkfb5icX9CXnKMpTopu9F2vymBWdV2qGt'
 heading_font = ImageFont.truetype(
     './fonts/Inter Hinted for Windows/Desktop/Inter-Bold.ttf', 70)
 normal_font = ImageFont.truetype(
@@ -34,5 +39,11 @@ for l in lines:
 
     i += 1
 
+qr = qrcode.QRCode(box_size=3)
+qr.add_data(qr_link)
+img_qr = qr.make_image(image_factory=StyledPilImage, module_drawer=RoundedModuleDrawer(), color_mask=SolidFillColorMask(back_color=(255,255,255),front_color=(41, 119, 245)))
+pos = (W - img_qr.width-135, H - img_qr.height-50)
+img_qr = img_qr.resize((150,150),Image.ANTIALIAS)
+certi_template.paste(img_qr, pos)
 
 certi_template.show()
